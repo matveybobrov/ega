94d06cd4ab57db12ac8c090c9796b07c8d86d22d
matveybobrov <matveybobrov01@gmail.com>
2023-09-17 17:41:33 +0300

    lab2: minor changes

--- lab2/index.js
+++ lab2/index.js
@@ -3,21 +3,20 @@ const L = 5
 // Получаем значение из командной строки
 const N = process.argv[2]
 const excludedCodes = []
+const code = generateCode()
+const prisp = getPrispByCode(code)
+let max = prisp
+let maxS = code
+
+let gamma = ''
 
 console.log('Ландшафт в формате "кодировка: приспособленность":')
 for (let i = 0; i < 32; i++) {
-  const currentCode = generateCode()
-  const currentPrisp = getPrispByCode(currentCode)
-  console.log(`${currentCode} : ${currentPrisp}`)
+  const generatedCode = generateCode()
+  const generatedPrisp = getPrispByCode(generatedCode)
+  console.log(`${generatedCode} : ${generatedPrisp}`)
 }
 console.log('--------------')
-
-// Генерируем начальную кодировку
-const code = generateCode()
-const prisp = getPrispByCode(code)
-let gamma = ''
-let max = prisp
-let maxS = code
 
 while (i < N) {
   console.log(`Номер шага: ${i}`)
@@ -50,12 +49,14 @@ while (i < N) {
 function generateCode() {
   let S = Array(L).fill(1)
   // Math.random() генерирует число в диапазоне [0,1), а Math.floor() округляет в меньшую сторону
-  S = S.map((item) => Math.floor(Math.random() * 2)).join('')
+  S = S.map(() => Math.floor(Math.random() * 2)).join('')
   return S
 }
 
 function getPrispByCode(code) {
-  return parseInt(code, 2)
+  const x = parseInt(code, 2)
+  const prisp = Math.pow(x - Math.pow(2, L - 1), 2)
+  return prisp
 }
 
 function getArea(code) {
