5d7610fe1dbaf9faeadc44f5c25ba8e3ce831885
Matvey <matveybobrov01@gmail.com>
2023-11-14 15:57:59 +0300

    lab8: implement basic functionality

---
+++ lab8/index.js
@@ -0,0 +1,67 @@
+import printArray from './helpers/printArray.js'
+import getArrayFromFile from './helpers/getArrayFromFile.js'
+
+const items = getArrayFromFile(`table5.txt`)
+const maxWeight = 35
+
+printArray(items)
+console.log(`Вместимость: ${maxWeight}`)
+
+let totalWeight = 0
+let totalValue = 0
+let result = Array(items.length).fill(0)
+
+for (let i = 0; i < items.length; i++) {
+  console.log(`\nШаг номер ${i + 1}`)
+
+  console.log(`Ищем предмет с максимальной удельной ценностью из оставшихся...`)
+  let bestItem = getMostValuableItem(items)
+  if (bestItem === null) {
+    console.log(`Не осталось предметов, которые бы влезли в рюкзак`)
+    break
+  }
+
+  console.log(`Лучший предмет ${bestItem.id + 1}`)
+  console.log(`Стоимость: ${bestItem.value}; Вес: ${bestItem.weight}`)
+  console.log(`Удельная ценность: ${(bestItem.value / bestItem.weight).toFixed(3)}`)
+
+  result[bestItem.id] = 1
+  totalWeight += bestItem.weight
+  totalValue += bestItem.value
+  console.log(`Решение изменилось на `, result)
+  console.log(`Вес ранца: ${totalWeight}/${maxWeight}`)
+  console.log(`Ценность ранца: ${totalValue}`)
+}
+
+console.log(`\nРешение: `, result)
+printArray(items.filter((item) => result[item.id] === 1))
+console.log(`Итоговый вес ранца: ${totalWeight}/${maxWeight}`)
+console.log(`Итоговая ценность ранца: ${totalValue}`)
+
+// Рекурсивная функция
+function getMostValuableItem(items) {
+  // Исключаем уже взятые предметы
+  items = items.filter((item) => result[item.id] !== 1)
+
+  let noMoreItems = items.length === 0
+  if (noMoreItems) {
+    return null
+  }
+
+  let unitValues = items.map((item) => {
+    return Number((item.value / item.weight).toFixed(3))
+  })
+  let mostValuableItemValue = Math.max(...unitValues)
+  let indexOfMostValuableItem = unitValues.indexOf(mostValuableItemValue)
+
+  let mostValuableItem = items[indexOfMostValuableItem]
+
+  let itemDoesntFit = mostValuableItem.weight > maxWeight - totalWeight
+  if (itemDoesntFit) {
+    let itemsWithoutCurrent = items.filter((item) => item.id !== mostValuableItem.id)
+    return getMostValuableItem(itemsWithoutCurrent)
+  }
+
+  let foundItem = items[indexOfMostValuableItem]
+  return foundItem
+}
